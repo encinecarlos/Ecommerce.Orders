5627da1d2c1127a4f95f500147d1e2d966c3eb52
Carlos Encine <carlos.encine@outlook.com>
2023-01-17 06:59:37 -0300

    feat: finish mongodb service implementation

--- apps/Orders-email/src/Order.Email.Api/Domain/Services/MongoDbClientService.cs
+++ apps/Orders-email/src/Order.Email.Api/Domain/Services/MongoDbClientService.cs
@@ -30,28 +30,34 @@ public class MongoDbClientService<T, TId> : IMongoDbClientService<T, TId>
         _collection = _client.GetDatabase(config.Value.Database).GetCollection<T>(collectionName);
     }
     
-    public Task InsertAsync(T entity)
+    public async Task InsertAsync(T entity)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Save data on database.");
+        await _collection.InsertOneAsync(entity);
     }
 
-    public Task<ReplaceOneResult> UpdateAsync(TId id, T entity)
+    public async Task<ReplaceOneResult> UpdateAsync(TId id, T entity)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"update document wuth id: {id}");
+        var filter = Builders<T>.Filter.Eq("_id", id);
+        return await _collection.ReplaceOneAsync(filter, entity);
     }
 
-    public Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
+    public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Get all documents from database");
+        return await (await _collection.FindAsync(predicate)).ToListAsync();
     }
 
-    public Task<T> FindSingleAsync(Expression<Func<T>> predicate)
+    public async Task<T> FindSingleAsync(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("find single record.");
+        return await (await _collection.FindAsync(predicate)).FirstOrDefaultAsync();
     }
 
-    public Task Removeasync(TId id)
+    public async Task Removeasync(TId id)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Remove document with id: {id}");
+        await _collection.DeleteOneAsync(Builders<T>.Filter.Eq("_id", id));
     }
 }
