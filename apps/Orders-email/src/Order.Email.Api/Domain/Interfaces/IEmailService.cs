623f8096c0654dc381d38c283b702dd7b156aff5
Carlos Encine <carlos.encine@outlook.com>
2023-01-21 20:36:38 -0300

    fat: start implementation of email delivery

---
+++ apps/Orders-email/src/Order.Email.Api/Domain/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Order.Email.Api.Domain.Interfaces;
+
+public interface IMessageHandlerService
+{
+    Task SendMessage(Entities.Order order);
+}
---
+++ apps/Orders-email/src/Order.Email.Api/Domain/Services/Messagehandler/MessageHandlerService.cs
@@ -0,0 +1,11 @@
+﻿using Order.Email.Api.Domain.Interfaces;
+
+namespace Order.Email.Api.Domain.Services.Messagehandler;
+
+public class MessageHandlerService : IMessageHandlerService
+{
+    public Task SendMessage(Entities.Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
